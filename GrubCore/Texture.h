35a6c5654fb4b4583a51e22e187513ae5a4ed322
Hologuardian <namaeskram@gmail.com>
2016-11-14 09:48:08 -0500

    Added a data vector to the texture class.

--- GrubCore/Texture.h
+++ GrubCore/Texture.h
@@ -1,10 +1,12 @@
 #ifndef GRUB_TEXTURE
 #define GRUB_TEXTURE
 #include "Resource.h"
+#include "TextureVertex.h"
 
 class Texture : public Resource<Texture>
 {
 public:
+	std::vector<TextureVertex> data;
 	Texture Load(std::string file) override
 	{
 		;
